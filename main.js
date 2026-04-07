b3d71fab74cf615d2b1b18ed4629dcba58d2419c
Emelinur <emelinuryzgc@gmail.com>
2026-04-07 17:58:12 +0300

    feat: enhance form validation feedback and styling for improved user interaction

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const submitBtn = document.querySelector(".submit-btn");
 const contactForm = document.querySelector(".contact-form");
 const InputAll = document.querySelectorAll("input");
-const errorMessage = document.querySelector(".error-message");
-const errorRadioButton = document.querySelectorAll(".radio-card");
-const errorTextArea = document.querySelector("textarea");
+const formGroupInput=document.querySelectorAll(".form-group__input");
+
+formGroupInput.forEach((btn)=>{
+  
+  btn.addEventListener("click",(e)=>{
+    e.target.classList.add("correct")
+
+  })
+})
+
+
 const isRadioSelected = document.querySelector(
   'input[name="query-type"]:checked',
 );
 const radioCards = document.querySelectorAll(".radio-card");
 radioCards.forEach((card) => {
+  
   card.addEventListener("click", () => {
     radioCards.forEach(c => c.classList.remove("radio-card-selected"));
-
     card.classList.add("radio-card-selected");
     const radioInput = card.querySelector('input[type="radio"]');
     if (radioInput) radioInput.checked = true;
@@ -23,9 +31,11 @@ submitBtn.addEventListener("click", () => {
     const errorMsg = input.parentElement.querySelector(".error-message");
     if (input.value.trim() === "") {
       input.classList.add("error");
+      input.classList.remove("correct")
       if (errorMsg) errorMsg.style.display = "block";
     } else {
       input.classList.remove("error");
+      input.classList.add("correct")
       if (errorMsg) errorMsg.style.display = "none";
     }
   });
@@ -37,10 +47,12 @@ submitBtn.addEventListener("click", () => {
 
   if (!isRadioSelected) {
     radioCards.forEach((card) => card.classList.add("error"));
+    radioCards.forEach((card) => card.classList.remove("correct"));
     if (radioErrorMsg) radioErrorMsg.style.display = "block";
     isValid = false;
   } else {
     radioCards.forEach((card) => card.classList.remove("error"));
+    radioCards.forEach((card) => card.classList.add("correct"));
     if (radioErrorMsg) radioErrorMsg.style.display = "none";
   }
   const inputsAndTextarea = document.querySelectorAll(
@@ -51,9 +63,11 @@ submitBtn.addEventListener("click", () => {
 
     if (field.value.trim() === "") {
       field.classList.add("error");
+       field.classList.remove("correct");
       if (errorMsg) errorMsg.style.display = "block";
     } else {
       field.classList.remove("error");
+      field.classList.add("correct");
       if (errorMsg) errorMsg.style.display = "none";
     }
   });
